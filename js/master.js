b1752332f1d02275ddc602e301ba281dcf461a93
JulieMarin <jmf7391@gmail.com>
2018-04-29 23:49:40 -0400

    Migrate project files to repo

---
+++ js/master.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  window.anchorHidden = false
+
+  // $(window).scroll(function() {
+  //   if ($(this).scrollTop() > 400) {
+  //     $('#menu-alt').fadeIn();
+  //   } else {
+  //     $('#menu-alt').fadeOut();
+  //   }
+  // });
+
+  $(window).stellar();
+
+  $('.elfsight-app-152f6ed5-1062-4f25-9367-55b90529b66b')
+  function hideAnchor() {
+    window.setTimeout(function() {
+      $('a[href^="https://elfsight.com/instagram-feed-instashow/?utm_source=websites&utm_medium=clients&utm_content=instashow&utm_term=&utm_campaign=free-plan"]').hide()
+    }, 2000);
+  }
+
+  hideAnchor()
+});
